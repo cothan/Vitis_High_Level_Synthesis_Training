8de0459cfc076d34c3e3eab8351667599af1d12a
cothan <cothannguyen@gmail.com>
2021-04-13 05:45:40 -0400

    add neon_code

--- neon/vector_mul.c
+++ neon/vector_mul.c
@@ -2,31 +2,48 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/* Compile flag
+ * gcc vector_mul.c -o out -O3 -fno-tree-vectorize -g3 -Wall -Wextra -Wpedantic
+ */
+
 #define vload(a, ptr) a = vld1q_u32_x4(ptr);
+#define vstore(ptr, a) vst1q_u32_x4(ptr, a);
 #define vmla(res, a, b, c) res = vmlaq_u32(a, b, c);
 #define vadd(c, a, b) c = vaddq_u32(a, b);
 #define vaddv(res, c) res = vaddvq_u32(c);
 #define vand(c, a, b) c = vandq_u32(a, b);
 
-#define N 200
+#define N 400
 #define ALPHA 2840173201 // 0b10101001010010011001101010010001
+
+void print_vectors(uint32x4x4_t v)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            printf("%8x - ", v.val[i][j]);
+        }
+        printf("\n");
+    }
+}
 
 uint32_t neon_vector_mul(const uint32_t *a, const uint32_t *b, const uint32_t *c)
 {
     // Total SIMD usage: 21
-    uint32x4x4_t neon_a, neon_b, neon_c, neon_e, neon_tmp;
-    uint32x4_t neon_mask;
-    uint32_t sum_e[4], sum = 0;
+    uint32x4x4_t neon_a, neon_b, neon_c, neon_e, neon_tmp; // 20
+    uint32x4_t neon_mask;                                  // 1
+    uint32_t sum;
 
     neon_e.val[0] = vdupq_n_u32(0);
     neon_e.val[1] = vdupq_n_u32(0);
     neon_e.val[2] = vdupq_n_u32(0);
     neon_e.val[3] = vdupq_n_u32(0);
 
-    const uint32_t mask = 0x1fff1fff;
+    const uint32_t mask = 0x1fffffff;
     neon_mask = vdupq_n_u32(mask);
 
-    for (int i = 0; i < N; i += 4 * 4)
+    for (int i = 0; i < N; i += 16)
     {
         vload(neon_a, &a[i]);
         vload(neon_b, &b[i]);
@@ -50,16 +67,12 @@ uint32_t neon_vector_mul(const uint32_t *a, const uint32_t *b, const uint32_t *c
         vadd(neon_e.val[2], neon_e.val[2], neon_tmp.val[2]);
         vadd(neon_e.val[3], neon_e.val[3], neon_tmp.val[3]);
     }
+    // e += tmp
+    vadd(neon_e.val[0], neon_e.val[0], neon_e.val[1]);
+    vadd(neon_e.val[0], neon_e.val[0], neon_e.val[2]);
+    vadd(neon_e.val[0], neon_e.val[0], neon_e.val[3]);
 
-    vaddv(sum_e[0], neon_e.val[0]);
-    vaddv(sum_e[1], neon_e.val[1]);
-    vaddv(sum_e[2], neon_e.val[2]);
-    vaddv(sum_e[3], neon_e.val[3]);
-
-    sum += sum_e[0];
-    sum += sum_e[1];
-    sum += sum_e[2];
-    sum += sum_e[3];
+    vaddv(sum, neon_e.val[0]);
 
     sum &= mask;
     sum *= ALPHA;
@@ -70,10 +83,10 @@ uint32_t neon_vector_mul(const uint32_t *a, const uint32_t *b, const uint32_t *c
 uint32_t vector_mul(const uint32_t *a, const uint32_t *b, const uint32_t *c)
 {
     uint32_t sum = 0;
-    const uint32_t mask = 0x1fff1fff;
+    const uint32_t mask = 0x1fffffff;
     for (int i = 0; i < N; i++)
     {
-        sum += (c[i] + a[i] * b[i]) & mask;
+        sum += (a[i] * b[i] + c[i]) & mask;
     }
     sum &= mask;
     sum *= ALPHA;
@@ -97,9 +110,9 @@ int main()
     e_gold = vector_mul(a, b, c);
     e_neon = neon_vector_mul(a, b, c);
 
-    int ret = e_gold == e_neon;
+    printf("%d -- %d\n", e_gold, e_neon);
 
-    if (ret)
+    if (e_gold != e_neon)
     {
         printf("Error\n");
         return 1;
